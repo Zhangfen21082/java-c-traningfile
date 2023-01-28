6f4be99ec9fc003a4f2eac8ba385d0c33bc22f43
Zhangfen21082 <290440677@qq.com>
2023-01-28 19:19:12 +0800

    JavaScript学习3之WebAPI

--- JavaEE-learning/J2023_01_24/src/javascript.js
+++ JavaEE-learning/J2023_01_24/src/javascript.js
@@ -1,3 +1,14 @@
-var a = null;
+function Student(name, age, content) {
+    this.name = name;
+    this.age = age;
+    this.say = function (){
+        console.log(this.name, content);
+    }
+}
 
-console.log(a);
+var obj1 = new Student("张三", 18, "你好");
+var obj2 = new Student("李四", 28, "拜拜");
+var obj3 = new Student("王五", 21, "好的");
+
+console.log(obj1);
+obj1.say();
